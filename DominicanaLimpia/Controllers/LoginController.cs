2fd7841dd34fed534700b0e8d7d2703aad46fda9
Williams Oleaga <oleaga29@gmail.com>
2019-06-28 14:44:00 -0400

    espacios

--- DominicanaLimpia/Controllers/LoginController.cs
+++ DominicanaLimpia/Controllers/LoginController.cs
@@ -11,7 +11,7 @@ namespace DominicanaLimpia.Controllers
         // GET: Login
         public ActionResult Index()
         {
-            return View();
+            return View();  
         }
     }
 }
