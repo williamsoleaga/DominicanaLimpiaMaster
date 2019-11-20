c002d30ef38e04a47dabc91771d9c22bc19d4103
woleaga <oleaga29@gmail.com>
2019-11-20 11:19:49 -0400

    numeracion de los reportes

--- DominicanaLimpia/Controllers/HomeController.cs
+++ DominicanaLimpia/Controllers/HomeController.cs
@@ -38,44 +38,44 @@ namespace DominicanaLimpia.Controllers
         {
             List<Combo> Combo = new List<Combo>();
 
-            Combo.Add(new Combo { Indice = 1, Value = "METAS POBLACION EN GENERAL" });
-            Combo.Add(new Combo { Indice = 2, Value = "METAS DE HOGARES"});
-            Combo.Add(new Combo { Indice = 3, Value = "METAS DE COMERCIOS"});
-            Combo.Add(new Combo { Indice = 4, Value = "METAS DE ESCUELAS"});
-            Combo.Add(new Combo { Indice = 5, Value = "METAS DE ESTUDIANTES" });
-            Combo.Add(new Combo { Indice = 6, Value = "METAS DOCENTES Y PERSONAL ADMINISTRATIVO"});
-            Combo.Add(new Combo { Indice = 7, Value = "METAS POBLACIÓN ESCUELAS" });
-            Combo.Add(new Combo { Indice = 8, Value = "METAS PUNTOS LIMPIOS" });
-            Combo.Add(new Combo { Indice = 9, Value = "META DOCENTES" });
-            Combo.Add(new Combo { Indice = 10, Value = "META ESTUDIANTES INICIAL" });
-            Combo.Add(new Combo { Indice = 11, Value = "META ESTUDIANTES PRIMARIA" });
-            Combo.Add(new Combo { Indice = 12, Value = "META ESTUDIANTES SECUNDARIA" });
-            Combo.Add(new Combo { Indice = 13, Value = "PARTICIPANTES JUNTAS DE VECINOS" });
-            Combo.Add(new Combo { Indice = 14, Value = "PARTICIPANTES DE COMUNIDAD EN GENERAL" });
-            Combo.Add(new Combo { Indice = 15, Value = "PARTICIPANTES DIASESOL" });
-            Combo.Add(new Combo { Indice = 16, Value = "DIASESOL PLASTICOS" });
-            Combo.Add(new Combo { Indice = 17, Value = "DIASESOL NEUMATICOS" });
-            Combo.Add(new Combo { Indice = 18, Value = "TALLERES DE RECICLAJE" });
-            Combo.Add(new Combo { Indice = 19, Value = "PUNTOS LIMPIOS COMUNITARIOS" });
-            Combo.Add(new Combo { Indice = 20, Value = "RECICLADORES CAPACITADOS" });
+            Combo.Add(new Combo { Indice = 1, Value = "1- METAS POBLACION EN GENERAL" });
+            Combo.Add(new Combo { Indice = 2, Value = "2- METAS DE HOGARES" });
+            Combo.Add(new Combo { Indice = 3, Value = "3- METAS DE COMERCIOS" });
+            Combo.Add(new Combo { Indice = 4, Value = "4- METAS DE ESCUELAS"});
+            Combo.Add(new Combo { Indice = 5, Value = "5- METAS DE ESTUDIANTES" });
+            Combo.Add(new Combo { Indice = 6, Value = "6- METAS DOCENTES Y PERSONAL ADMINISTRATIVO"});
+            Combo.Add(new Combo { Indice = 7, Value = "7- METAS POBLACIÓN ESCUELAS" });
+            Combo.Add(new Combo { Indice = 8, Value = "8- METAS PUNTOS LIMPIOS" });
+            Combo.Add(new Combo { Indice = 9, Value = "9- META DOCENTES" });
+            Combo.Add(new Combo { Indice = 10, Value = "10- META ESTUDIANTES INICIAL" });
+            Combo.Add(new Combo { Indice = 11, Value = "11- META ESTUDIANTES PRIMARIA" });
+            Combo.Add(new Combo { Indice = 12, Value = "12- META ESTUDIANTES SECUNDARIA" });
+            Combo.Add(new Combo { Indice = 13, Value = "13- PARTICIPANTES JUNTAS DE VECINOS" });
+            Combo.Add(new Combo { Indice = 14, Value = "14- PARTICIPANTES DE COMUNIDAD EN GENERAL" });
+            Combo.Add(new Combo { Indice = 15, Value = "15- PARTICIPANTES DIASESOL" });
+            Combo.Add(new Combo { Indice = 16, Value = "16- DIASESOL PLASTICOS" });
+            Combo.Add(new Combo { Indice = 17, Value = "17- DIASESOL NEUMATICOS" });
+            Combo.Add(new Combo { Indice = 18, Value = "18- TALLERES DE RECICLAJE" });
+            Combo.Add(new Combo { Indice = 19, Value = "19- PUNTOS LIMPIOS COMUNITARIOS" });
+            Combo.Add(new Combo { Indice = 20, Value = "20- RECICLADORES CAPACITADOS" });
 
-            Combo.Add(new Combo { Indice = 21, Value = "ENTREGA DE MONTA CARGAS" });
-            Combo.Add(new Combo { Indice = 22, Value = "RECICLADORES IDENTIFICADOS" });
-            Combo.Add(new Combo { Indice = 23, Value = "EMPRESAS RECICLADORAS" });
-            Combo.Add(new Combo { Indice = 24, Value = "CAPACITADOS BRIGADAS" });
-            Combo.Add(new Combo { Indice = 25, Value = "CAPACITADOS ICAM" });
-            Combo.Add(new Combo { Indice = 26, Value = "VERTEDEROS ACTUALES" });
-            Combo.Add(new Combo { Indice = 27, Value = "CAMIONES DE RECOLECCION SEPARADAS" });
-            Combo.Add(new Combo { Indice = 28, Value = "VERTEDEROS ELIMINADOS" });
-            Combo.Add(new Combo { Indice = 29, Value = "VERTEDEROS NUEVOS" });
-            Combo.Add(new Combo { Indice = 30, Value = "CENTRO DE ACOPIOS FUNCIONANDO" });
-            Combo.Add(new Combo { Indice = 31, Value = "CENTRO DE ACOPIOS CONSTRUIDO" });
-            Combo.Add(new Combo { Indice = 32, Value = "CAPACITADOS CENTROS DE ACOPIOS" });
-            Combo.Add(new Combo { Indice = 33, Value = "ABONERAS FUNCIONANDO" });
+            Combo.Add(new Combo { Indice = 21, Value = "21- ENTREGA DE MONTA CARGAS" });
+            Combo.Add(new Combo { Indice = 22, Value = "22- RECICLADORES IDENTIFICADOS" });
+            Combo.Add(new Combo { Indice = 23, Value = "23- EMPRESAS RECICLADORAS" });
+            Combo.Add(new Combo { Indice = 24, Value = "24- CAPACITADOS BRIGADAS" });
+            Combo.Add(new Combo { Indice = 25, Value = "25- CAPACITADOS ICAM" });
+            Combo.Add(new Combo { Indice = 26, Value = "26- VERTEDEROS ACTUALES" });
+            Combo.Add(new Combo { Indice = 27, Value = "27- CAMIONES DE RECOLECCION SEPARADAS" });
+            Combo.Add(new Combo { Indice = 28, Value = "28- VERTEDEROS ELIMINADOS" });
+            Combo.Add(new Combo { Indice = 29, Value = "29- VERTEDEROS NUEVOS" });
+            Combo.Add(new Combo { Indice = 30, Value = "30- CENTRO DE ACOPIOS FUNCIONANDO" });
+            Combo.Add(new Combo { Indice = 31, Value = "31- CENTRO DE ACOPIOS CONSTRUIDO" });
+            Combo.Add(new Combo { Indice = 32, Value = "32- CAPACITADOS CENTROS DE ACOPIOS" });
+            Combo.Add(new Combo { Indice = 33, Value = "33- ABONERAS FUNCIONANDO" });
 
             //no se sabe con que es que se hace ese calculo
-            Combo.Add(new Combo { Indice = 34, Value = "ABONERAS CONSTRUIDAS" });
-            Combo.Add(new Combo { Indice = 35, Value = "CAPACITADOS ABONERAS" });
+            Combo.Add(new Combo { Indice = 34, Value = "34- ABONERAS CONSTRUIDAS" });
+            Combo.Add(new Combo { Indice = 35, Value = "35- CAPACITADOS ABONERAS" });
 
 
             return Combo;
